ad2b178fbffdfd61858e7716f99e280670d545c4
Vladimir <39053957+Vladimir-Human@users.noreply.github.com>
2026-09-12 16:13:50 +0300

    Require confirmation before sharing demo text (#237)

--- demo/sw.js
+++ demo/sw.js
@@ -1,5 +1,5 @@
 /* Автогенерация generate_js_rules.py: кэш версионируется хэшем правил, движка, образца и страницы. */
-const CACHE = "humanizer-ru-99bc3bf907e1";
+const CACHE = "humanizer-ru-0bc9612a909d";
 const STATIC = ["./", "./index.html", "./brand.css", "./markers.js",
   "./engine.js", "./sample.js", "./favicon.svg", "./manifest.json"];
 self.addEventListener("install", (e) => {
